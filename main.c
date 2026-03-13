e4f5686abb940fbe1e51c65f6e8f9d8610c23e1c
mhdpro1 <mhdep9474@gmail.com>
2026-03-13 20:36:46 +0000

    add commit

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-
+hi
 
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+    int main(int argc, char *argv[]) {
     printf("hi");
     // d
     return EXIT_SUCCESS;
